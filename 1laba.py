6036fd276c0a400195c4a8bf1e0c891b9ba6e516
MegaDig <x_digital_x@mail.ru>
2021-06-15 22:26:14 +1000

    Update 1laba.py

--- 1laba.py
+++ 1laba.py
@@ -1,4 +1,4 @@
-﻿import csv
+import csv
 day = 8
 month = 3
 variant = (5 + day) * 2 + month
@@ -8,22 +8,22 @@ calls = {}
 max_number = 0
 max_call = 0
 output_filename = 'output_' + str(variant) + '.csv'
-with open('/home/marta/input.csv', 'r') as r_file:
-	readerCRP = csv.DictReader(r_file, delimiter=';', fieldnames=['calling phone', 'receiving phone', 'date', 'price'])
-	with open(output_filename, 'w') as w_file:
-		file_writer = csv.DictWriter(w_file, delimiter=';', lineterminator='\r', fieldnames=['calling phone', 'receiving phone', 'date', 'price'])
-	for row in readerCRP:
-		if (row['calling phone']) == variant:
-			file_writer.writerow(row)
-	with open(output_filename, 'r') as s_file:
-		readerCRP = csv.DictReader(s_file, delimiter=';', fieldnames=['calling phone', 'receiving phone', 'date', 'price'])
-	for row in readerCRP:
-		fullprice = fullprice + int(row['price'])
-		calls[row['receiving phone']] = calls.get(row['receiving phone'], 0) + 1
-		for k, v in calls.items():
-			if v > max_call:
-				max_number = k
-				max_call = v
-				print('Цена за звонки: %s' % fullprice)
-				print('Наиболее частый вызываемый номер: %s' % max_number)
-				print('Количество звонков часто вызываемого номера: %s' % max_call)
+with open('input.csv', 'r') as r_file:
+    readerCRP = csv.DictReader(r_file, delimiter=';', fieldnames=['calling phone', 'receiving phone', 'date','price'])
+    with open(output_filename, 'w') as w_file:
+        file_writer = csv.DictWriter(w_file, delimiter=';', lineterminator='\r', fieldnames=['calling phone', 'receiving phone', 'date', 'price'])
+        for row in readerCRP:
+                if (row['calling phone']) == str(variant):
+                    file_writer.writerow(row)
+    with open(output_filename, 'r') as s_file:
+        readerCRP = csv.DictReader(s_file, delimiter=';', fieldnames=['calling phone', 'receiving phone', 'date', 'price'])
+        for row in readerCRP:
+            fullprice = fullprice + int(row['price'])
+            calls[row['receiving phone']] = calls.get(row['receiving phone'], 0) + 1
+            for k, v in calls.items():
+                if v > max_call:
+                    max_number = k
+                    max_call = v
+                    print('Цена за звонки: %s' % fullprice)
+                    print('Наиболее частый вызываемый номер: %s' % max_number)
+                    print('Количество звонков часто вызываемого номера: %s' % max_call)
